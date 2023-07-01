5d320a1e2a70c62554c55b1b39e8e6ede9a24eec
unknown <100839060+StephenC45@users.noreply.github.com>
2023-07-02 08:34:04 +1000

    Added stub for edit page.

--- src/pages/EditPage.js
+++ src/pages/EditPage.js
@@ -2,10 +2,45 @@
 
 
 function EditPage() {
+  const itineraryList = [
+    {
+      title: "Nature in Sydney",
+      location: "Sydney",
+      nights: 4,
+      stops: [
+        // Add stops here.
+      ],
+      profileName: "John"
+    }
+    // Add more sample itineraries here.
+  ]
+
   // TODO: hard code at least one itinerary.
   return (
     <>
       Hello, world! This is the edit page. The user can select one of their itineraries to edit from here.
+      {/* Map itineraries... */}
+      {
+        itineraryList.map((i, index) => {
+          return (
+            <div>
+              <div style={{
+                // Add styles for itinarary title here.
+              }}
+              >
+                {/* Itinerary title */}
+              </div>
+
+              <div style={{
+                // Add styles for itinarary details (location, duration, etc.) here.
+              }}
+              >
+                {/* Itinerary details */}
+              </div>
+            </div>
+          )
+        })
+      }
     </>
   )
 }
