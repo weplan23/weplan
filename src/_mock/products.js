e705467560e5f0fc9634643d6e135205a01bd499
Conrad Vernon <conradvernon@Conrads-MacBook-Air.local>
2023-07-02 10:50:02 +1000

    "added updoots"

--- src/_mock/products.js
+++ src/_mock/products.js
@@ -17,6 +17,9 @@ const PRODUCT_NAME = [
   "Pack your garlic - Transylvania",
   "Hippopotamus or hippocampus?",
   "Deal or no deal Mr Giza",
+  "Sandy Shores in Brazil",
+  "All Thai-ed up",
+  "Ireland."
 ];
 const IMAGES = [
   "https://thumbs.dreamstime.com/b/big-ben-london-autumn-leaves-32915756.jpg",
@@ -33,6 +36,9 @@ const IMAGES = [
   "https://lp-cms-production.imgix.net/features/2015/08/BranCastle_Transylvania-d91a08d52fa2.jpg?w=600&h=400",
   "https://www.grantordelheide.com/images/xl/Ordelheide_20161207-_N4A7272_breachinghippo.jpg",
   "https://i.natgeofe.com/n/535f3cba-f8bb-4df2-b0c5-aaca16e9ff31/giza-plateau-pyramids.jpg",
+  "https://lp-cms-production.imgix.net/2021-10/shutterstockRF_477734146.jpg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_Ysj3bhdOpL0q9i2JAclxZCddTB2fdqIxXQ&usqp=CAU",
+  "https://www.connollycove.com/wp-content/uploads/2019/06/temple-bar-dublin-world-famous-irish-pub.jpg",
   "Cairo, Egypt",
   "Rio de Janeiro, Brazil",
   "Toronto, Canada",
@@ -51,8 +57,31 @@ const IMAGES = [
   "Bangkok, Thailand"
 ];
 const UPVOTES = [
+  3,
+  5,
+  0,
+  2,
+  1,
+  0,
+  3,
+  4,
+  5,
+  2,
+  2,
+  1,
+  0,
+  3,
+  0,
+  0
 ]
 const PROFILE_IMAGES = [
+  "https://expertphotography.b-cdn.net/wp-content/uploads/2018/10/cool-profile-pictures-retouching-1.jpg",
+  "https://www.mwakilishi.com/sites/default/files/styles/large/public/2020-05/ndiritu.PNG?itok=pYNW9js5",
+  "https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/01/31/16/wang-qi.jpg",
+  "https://img.freepik.com/premium-photo/kazakh-man-with-falcon_672794-1049.jpg",
+  "https://blog-6aa0.kxcdn.com/wp-content/uploads/2022/03/nice-things-to-say-to-mum-cover.jpg",
+  "https://hips.hearstapps.com/hmg-prod/images/lesley-manville-mum-1557765393.jpg",
+  "https://resizing.flixster.com/uVt51cRU8H_EJkEEto-PMM6Ln6c=/300x300/v2/http://media.baselineresearch.com/images/317395/317395_full.jpg"
   
 ]
 const PRODUCT_COLOR = ['#00AB55', '#000000', '#FFFFFF', '#FFC0CB', '#FF4842', '#1890FF', '#94D82D', '#FFC107'];
